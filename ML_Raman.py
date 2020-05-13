6ef2e27af1565a51d3df57dacdcce0e98963c320
kevinmm6 <kevinmm@chem.ubc.ca>
2020-05-12 18:43:56 -0700

    Update ML_Raman.py

--- ML_Raman.py
+++ ML_Raman.py
@@ -45,13 +45,7 @@ def decision_tree(X,target_data,ii,target_name,dt_optimization_leafs,y,mae_dt):
         mae_dt['Burst_opt']= min(dtmae)
         mae_dt['Burst_nodes'] = dt_optimization_leafs[dtmae_min_idx]
     return mae_dt
-    # # Plotting 
-    # plt.figure(ii+1)
-    # plt.plot(dt_optimization_leafs,dtmae,'.') 
-    # plt.xlabel('Number of leafs')
-    # plt.ylabel('Mean Absolute Error')
-    # plt.title('Decision Tree Model for {}'.format(target_name))
-    
+
 # Random Forest 
 def random_forest(X,target_data,ii,target_name,rf_optimization_leafs,y,mae_rf):
     # Split data
@@ -83,13 +77,7 @@ def random_forest(X,target_data,ii,target_name,rf_optimization_leafs,y,mae_rf):
         mae_rf['Burst_opt']= min(rfmae)
         mae_rf['Burst_nodes'] = rf_optimization_leafs[rfmae_min_idx]
         return mae_rf
-    # # Plotting
-    # plt.figure(ii+1+target_data.shape[1])
-    # plt.plot(rf_optimization_leafs,rfmae,'.')
-    # plt.xlabel('Number of leafs')
-    # plt.ylabel('Mean Absolute Error')
-    # plt.title('Random Forest Model for {}'.format(target_name))
-
+    
 # Comparison
 def comparison(target_name,mae_dt,mae_rf):
     if target_name == 'BL':
@@ -176,6 +164,7 @@ rf_optimization_leafs = [20,22,24,28,30,32,34] #Number of optimization tree leaf
 #Initialize 
 mae_dt = {'Tear': 0,'burst': 0,'BL': 0,'Tear_nodes' : 0,'BL_nodes' : 0,'Burst_nodes' : 0}
 mae_rf = {'Tear': 0,'burst': 0,'BL': 0,'Tear_nodes' : 0,'BL_nodes' : 0,'Burst_nodes' : 0}
+#Script
 for ii, target_name in enumerate(target_data):
     y = np.array(target_data.iloc[:,ii]) #Target data
     decision_tree(X,target_data,ii,target_name,dt_optimization_leafs,y,mae_dt)
