3ee3126acd4680502ad3b331f70ef8becce3183e
kevinmm6 <kevinmm@chem.ubc.ca>
2020-06-06 16:12:40 -0700

    Update ML_Raman.py

--- ML_Raman.py
+++ ML_Raman.py
@@ -1,16 +1,10 @@
 # 10/03/20
 # Kevin Marroquin
-# Code using two different machine learning models (Decision Tree and Random 
-# Forest) on Raman data for pulp.
+# Code using machine learning models on Raman data for pulp.
 # %% Libraries setup
 import numpy as np
-import pandas as pd # Read data files (.csv or .xlsx)
+import pandas as pd 
 import matplotlib.pyplot as plt
-from numpy import arange
-from matplotlib import pyplot
-from pandas import set_option
-from pandas.plotting import scatter_matrix
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
@@ -21,14 +15,11 @@ from sklearn.linear_model import ElasticNet
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.svm import SVR
-from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_absolute_error #MAE
-import astropy.table as astrtable #Tables
 #%% Load and explore data
 feature_filename='Raman 20200218.csv'
 target_filename='Targets 20200218.csv'
@@ -43,10 +34,10 @@ target_data=target_dataset[target_names]
 dataset = pd.concat([feature_data,target_data],axis=1)
 #Drop NaN values
 dataset = dataset.dropna()
-#%% Set training and validation dataset
+#%% Set training and validation dataset with burst target
 X = dataset[spectra_names].values
 y = dataset["Burst"].values
-#Leave 20% pf data for validation purposes
+#Leave 20% of data for validation purposes
 validation_size=0.20
 seed = 7
 train_X,val_X, train_y, val_y = train_test_split(X,y,test_size=validation_size,random_state=seed)
@@ -75,15 +66,18 @@ for name,model in models:
 plt.figure(1)
 plt.boxplot(results,labels=model_names)
 #%% Tunning 
-#Although Lasso gave the best result it is not converging so we optimize SVR since it was the second best
-#SVR
-c_values=np.arange(10,100,20)
-kernel_values=["poly","rbf","sigmoid"]
-svr_grid_params={"C":c_values,"kernel":kernel_values}
-svr_model=SVR()
-svr_grid=GridSearchCV(estimator=svr_model,param_grid=svr_grid_params,scoring=scoring_metric,cv=kfold)
-svr_grid_result=svr_grid.fit(train_X,train_y)
-print("Best value: {} with {}".format(svr_grid_result.best_score_,svr_grid_result.best_params_))
+#KNN
+k_values=np.arange(1,21,2)
+knn_grid_params={"n_neighbors":k_values}
+knn_model=KNeighborsRegressor()
+knn_grid=GridSearchCV(estimator=knn_model,param_grid=knn_grid_params,scoring=scoring_metric,cv=kfold)
+knn_grid_results=knn_grid.fit(train_X,train_y)
+print("Best value: {} with {}".format(knn_grid_results.best_score_,knn_grid_results.best_params_))
+knn_grid_means=knn_grid_results.cv_results_['mean_test_score']
+knn_grid_std=knn_grid_results.cv_results_['std_test_score']
+knn_grid_params=knn_grid_results.cv_results_['params']
+for ii in range(0,knn_grid_means.size):
+    print('{:.5f} ({:.5f}) with {}'.format(knn_grid_means[ii],knn_grid_std[ii],knn_grid_params[ii]))
 #%% Ensembles
 ensembles=[]
 ensembles.append(('AB',AdaBoostRegressor()))
@@ -100,16 +94,21 @@ for en_name,en_model in ensembles:
 plt.figure(2)
 plt.boxplot(results_ensemble,labels=names_ensemble)
 #%% Ensembles Tunning
+#GB
 kfold=KFold(n_splits=n_folds,random_state=seed,shuffle=True)
-et_model=ExtraTreesRegressor()
-n_leafs=np.arange(8,15,2)
-n_est=np.arange(100,120,10)
-en_grid_params={"max_leaf_nodes":n_leafs,'n_estimators':n_est}
-en_grid=GridSearchCV(estimator=et_model,param_grid=en_grid_params,scoring=scoring_metric,cv=kfold)
-en_grid_result=en_grid.fit(train_X,train_y)
-print('Best value: {} with {}'.format(en_grid_result.best_score_,en_grid_result.best_params_))
+gb_model=GradientBoostingRegressor(random_state=seed)
+n_est=np.arange(100,150,10)
+gb_grid_params={'n_estimators':n_est}
+gb_grid=GridSearchCV(estimator=gb_model,param_grid=gb_grid_params,scoring=scoring_metric,cv=kfold)
+gb_grid_result=gb_grid.fit(train_X,train_y)
+print('Best value: {} with {}'.format(gb_grid_result.best_score_,gb_grid_result.best_params_))
+gb_grid_means=gb_grid_result.cv_results_['mean_test_score']
+gb_grid_std=gb_grid_result.cv_results_['std_test_score']
+gb_grid_params=gb_grid_result.cv_results_['params']
+for ii in range(0,gb_grid_means.size):
+    print('{} ({}) with {}'.format(gb_grid_means[ii],gb_grid_std[ii],gb_grid_params[ii]))
 #%% Finalize model
-final_model=ExtraTreesRegressor(n_estimators=100,max_leaf_nodes=100,random_state=seed)
+final_model=GradientBoostingRegressor(n_estimators=120,random_state=seed)
 final_model.fit(train_X,train_y)
 predictions=final_model.predict(val_X)
 print(mean_squared_error(val_y,predictions))
